d5a16dde155995525480762bd071c02b850cdd9c
praveenkumargunda <108858306+praveenkumargunda@users.noreply.github.com>
2025-09-22 15:37:46 +0530

    Created PurchaseException.java

---
+++ GHI-Final/src/main/java/com/resilenceindia/insurance/exception/PurchaseException.java
@@ -0,0 +1,7 @@
+package com.resilenceindia.insurance.exception;
+
+public class PurchaseException extends RuntimeException {
+    public PurchaseException(String message) {
+        super(message);
+    }
+}
