715c6843ffaca0a35a16d316230ab35ff8258de5
praveenkumargunda <108858306+praveenkumargunda@users.noreply.github.com>
2025-09-22 15:37:10 +0530

    Created PaymentException.java

---
+++ GHI-Final/src/main/java/com/resilenceindia/insurance/exception/PaymentException.java
@@ -0,0 +1,7 @@
+package com.resilenceindia.insurance.exception;
+
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
+}
